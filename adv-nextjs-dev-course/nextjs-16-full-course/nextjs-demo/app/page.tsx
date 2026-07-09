3337574294f84834b3961c98c4f910db20eb2293
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-07-09 20:23:10 +0600

    complete: React Client & Server Components.

--- adv-nextjs-dev-course/nextjs-16-full-course/nextjs-demo/app/page.tsx
+++ adv-nextjs-dev-course/nextjs-16-full-course/nextjs-demo/app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Hello from "./components/hello";
 
 const Home = () => {
-  return <div className="text-5xl underline">Welcome to Next.js!</div>;
+  console.log("What type of a component am I?");
+
+  return (
+    <main>
+      <div className="text-5xl underline">Welcome to Next.js!</div>
+      <Hello />
+    </main>
+  );
 };
 
 export default Home;
