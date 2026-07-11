b5f6effcb47a944eaaaca5474dd02df67ccf699b
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-07-12 01:13:46 +0600

    complete: Homepage.

--- adv-nextjs-dev-course/nextjs-16-full-course/nextjs-crash-course/app/page.tsx
+++ adv-nextjs-dev-course/nextjs-16-full-course/nextjs-crash-course/app/page.tsx
@@ -1,4 +1,6 @@
+import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
 
 const Page = () => {
   return (
@@ -16,9 +18,11 @@ const Page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
 
-        <ul className="events">
-          {[1, 2, 3, 4, 5].map((event) => (
-            <li key={event}>Event {event}</li>
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
           ))}
         </ul>
       </div>
